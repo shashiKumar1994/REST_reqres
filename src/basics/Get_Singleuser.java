f7c3234872f244932cc2ebe7be2300428f5556f6
shashikumar1994 <shashi18121994@gmail.com>
2020-09-23 21:29:37 +0530

    extracted some more node from response body

--- src/basics/Get_Singleuser.java
+++ src/basics/Get_Singleuser.java
@@ -17,6 +17,10 @@ public class Get_Singleuser {
 		 System.out.println(response);
 		 JsonPath jp=new JsonPath(response);
 		System.out.println(jp.getString("data.id"));
+		System.out.println(jp.getString("data.email"));
+		System.out.println(jp.getString("data.first_name"));
+		System.out.println(jp.getString("data.last_name"));
+		System.out.println(jp.getString("data.avatar"));
 
 	}
 
