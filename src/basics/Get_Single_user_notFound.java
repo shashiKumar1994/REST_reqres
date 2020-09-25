bf5d1ce970b7b9fd2798b9e17e87a40c363e88cc
shashikumar1994 <shashi18121994@gmail.com>
2020-09-25 13:25:54 +0530

    checked for error status code

---
+++ src/basics/Get_Single_user_notFound.java
@@ -0,0 +1,21 @@
+package basics;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class Get_Single_user_notFound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RestAssured.baseURI="https://reqres.in/";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("api/users/23");
+		ResponseBody body = response.getBody();
+		System.out.println(body.asString());
+
+	}
+
+}
